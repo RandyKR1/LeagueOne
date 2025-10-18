4c00e66d48904b6e55ab91c506b769031f5d3c1b
Randall Reichenberger <randall.reichenberger1@gmail.com>
2025-10-18 07:22:52 -0500

    Added Race model and schemas

---
+++ backend/models/race.js
@@ -0,0 +1,54 @@
+module.exports = (sequelize, DataTypes) => {
+    const Race = sequelize.define('Race', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        leagueId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Leagues',
+                key: 'id',
+            },
+            onDelete: 'CASCADE',
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        round: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.ENUM('Scheduled', 'Completed'),
+            allowNull: false,
+            defaultValue: 'Scheduled',
+        }
+    }, {
+        tableName: 'Races'
+    });
+
+    Race.associate = (models) => {
+        Race.belongsTo(models.League, {
+            as: 'league',
+            foreignKey: 'leagueId'
+        }),
+
+            Race.hasMany(models.RaceResult, {
+                foreignKey: 'raceId',
+                as: 'results',
+                onDelete: 'CASCADE',
+            });
+    }
+}
